2d36c919303f67bf0408365c91f29b9f35755d66
Yooooomi <timothee.boussus@gmail.com>
2018-01-08 19:51:09 -0800

    added weapon class

---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour {
+
+    public string Name;
+    public int clip_cap;
+    public int max_ammo;
+    public int clip_ammo;
+    public int ammo;
+    public float fire_rate;
+    public float reload_time;
+    public float range;
+
+    public void reload()
+    {
+        if (clip_ammo == clip_cap || ammo == 0)
+            return;
+        else if (ammo >= clip_cap - clip_ammo)
+        {
+            ammo -= clip_cap - clip_ammo;
+            clip_ammo = clip_cap;
+        } else if (ammo < clip_cap - clip_ammo) {
+            clip_ammo += ammo;
+            ammo = 0;
+        }
+    }
+
+}
