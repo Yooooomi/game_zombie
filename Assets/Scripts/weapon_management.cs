8173d9f69b87e15ef4ea6d2aa7957a0651fc6167
BuildTools <unconfigured@null.spigotmc.org>
2018-01-09 17:31:44 +0100

    added ammo dropping, ammo separated from weapons

--- Assets/Scripts/weapon_management.cs
+++ Assets/Scripts/weapon_management.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using System.Linq;
 
 public class weapon_management : MonoBehaviour {
+
+    private const int RIFLE_INDEX = 0;
+    private const int GUN_INDEX = 1;
+    private const int SHOTGUN_INDEX = 2;
 
     public data_center dc;
     public GameObject wp_obj;
@@ -12,11 +17,42 @@ public class weapon_management : MonoBehaviour {
     public List<weapon> weapons = new List<weapon>();
     private float time_since_last_shoot = 0;
     public GameObject weapon_box;
+    public List<int> ammos = new List<int>();
 
     // Use this for initialization
 	void Start () {
+        for (int i = 0; i < 3; i++)
+        {
+            ammos.Add(90);
+        }
         weapons.Add(new weapon());
         weapons.Add(new weapon());
+    }
+
+    public void add_ammos(int type, int amount)
+    {
+        ammos[type] += amount;
+    }
+
+    public bool reload()
+    {
+        weapon wp = weapons[index];
+
+        if (wp.clip_ammo == wp.clip_cap || ammos[wp.type] == 0)
+            return false;
+        else if (ammos[wp.type] >= wp.clip_cap - wp.clip_ammo)
+        {
+            ammos[wp.type] -= wp.clip_cap - wp.clip_ammo;
+            wp.clip_ammo = wp.clip_cap;
+        }
+        else if (ammos[wp.type] < wp.clip_cap - wp.clip_ammo)
+        {
+            wp.clip_ammo += ammos[wp.type];
+            ammos[wp.type] = 0;
+        }
+        dc.ui.refresh_weapon();
+        dc.ui.refresh_ammos();
+        return (true);
     }
 
     void drop_weapon()
@@ -74,9 +110,26 @@ public class weapon_management : MonoBehaviour {
         bool shooting = CrossPlatformInputManager.GetButton("Fire1");
         bool drop = CrossPlatformInputManager.GetButtonDown("Drop");
         float scroll = CrossPlatformInputManager.GetAxis("Mouse ScrollWheel");
+        bool reloading = CrossPlatformInputManager.GetButtonDown("Reload");
+
+        if (reloading)
+        {
+            if (reload())
+            {
+                time_since_last_shoot -= current_weapon.reload_time;
+            }
+        }
+        if (current_weapon.auto)
+        {
+            shooting = CrossPlatformInputManager.GetButton("Fire1");
+        }
+        else
+        {
+            shooting = CrossPlatformInputManager.GetButtonDown("Fire1");
+        }
 
         time_since_last_shoot += Time.deltaTime;
-        if (shooting && time_since_last_shoot > current_weapon.fire_rate && current_weapon.can_fire()) {
+        if (shooting && time_since_last_shoot > current_weapon.get_fire_rate() && current_weapon.can_fire()) {
             shoot(current_weapon);
             time_since_last_shoot = 0;
         }
